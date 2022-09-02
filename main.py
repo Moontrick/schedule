c07aac8ff400c931dad11e997cb46d0f8651444b
Moontrick <47784395+Moontrick@users.noreply.github.com>
2022-09-02 17:58:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ api = requests.get(url)
 tree = lxml.html.document_fromstring(api.text)
 text_teg = tree.xpath('//*[@id="1_1"]/div/div[2]/text()')
 print(text_teg)
-
+text_teg = tree.xpath('//*[@id="1_1"]/div/div[3]/a/text()')
+print(text_teg)
+text_teg = tree.xpath('//*[@id="1_1"]/div/div[4]/text()')
+print(text_teg)
 
 
 
