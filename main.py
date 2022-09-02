bf66ab308b3809e27272b8ad51fc7f756046e87b
Moontrick <47784395+Moontrick@users.noreply.github.com>
2022-09-02 18:17:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 import asyncio
 import requests
 from lxml import etree
@@ -9,12 +7,18 @@ import lxml.html
 url = "https://www.sgu.ru/schedule/knt/do/311"
 api = requests.get(url)
 tree = lxml.html.document_fromstring(api.text)
-text_teg = tree.xpath('//*[@id="1_1"]/div/div[2]/text()')
-print(text_teg)
-text_teg = tree.xpath('//*[@id="1_1"]/div/div[3]/a/text()')
-print(text_teg)
-text_teg = tree.xpath('//*[@id="1_1"]/div/div[4]/text()')
-print(text_teg)
-
+for i in range(6):
+    for j in range(8):
+        str1 = '//*[@id="' + str(j+1) + '_' + str(i+1)
+        text_teg = tree.xpath(str1 + '"]/div/div[1]/div[2]/text()')
+        print(text_teg)
+        text_teg = tree.xpath(str1 + '"]/div/div[1]/div[3]/text()')
+        print(text_teg)
+        text_teg = tree.xpath(str1 + '"]/div/div[2]/text()')
+        print(text_teg)
+        text_teg = tree.xpath(str1 + '"]/div/div[3]/a/text()')
+        print(text_teg)
+        text_teg = tree.xpath(str1 + '"]/div/div[4]/text()')
+        print(text_teg)
 
 
